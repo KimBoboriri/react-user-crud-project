12aea11ea12b2ce8344a1ca05481f1622e49db58
dongyoungkim <kdy41000@naver.com>
2022-04-11 23:22:38 +0900

    홈페이지 설정

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from 'react-router-dom';  //spa설정
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
   </BrowserRouter>
 );
